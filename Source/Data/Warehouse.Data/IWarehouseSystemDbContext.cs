57635424c5c19bb83f86451ff142d25753d0e653
NikitoG <niki_spike@yahoo.com>
2016-02-07 22:32:34 +0200

    Repository Pattern

--- Source/Data/Warehouse.Data/IWarehouseSystemDbContext.cs
+++ Source/Data/Warehouse.Data/IWarehouseSystemDbContext.cs
@@ -9,11 +9,29 @@ using Warehouse.Models;
 
 namespace Warehouse.Data
 {
-    interface IWarehouseSystemDbContext : IDisposable
+    public interface IWarehouseSystemDbContext : IDisposable
     {
         int SaveChanges();
 
         IDbSet<User> Users { get; set; }
+
+        IDbSet<Category> Categories { get; set; }
+
+        IDbSet<Division> Divisions { get; set; }
+
+        IDbSet<Image> Images { get; set; }
+
+        IDbSet<Message> Messages { get; set; }
+
+        IDbSet<OrderQuantity> OrderQuantities { get; set; }
+
+        IDbSet<Organization> Organizations { get; set; }
+
+        IDbSet<Product> Products { get; set; }
+
+        IDbSet<PurchaseOrder> PurchaseOrders { get; set; }
+
+        IDbSet<ScheduleOrder> ScheduleOrders { get; set; }
 
         DbSet<TEntity> Set<TEntity>() where TEntity : class;
 
--- Source/Data/Warehouse.Data/WarehouseSystemDbContext.cs
+++ Source/Data/Warehouse.Data/WarehouseSystemDbContext.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Identity.EntityFramework;
-using Warehouse.Models;
-
-namespace Warehouse.Data
+﻿namespace Warehouse.Data
 {
+    using System.Data.Entity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Warehouse.Models;
+
     public class WarehouseSystemDbContext : IdentityDbContext<User>, IWarehouseSystemDbContext
     {
         public WarehouseSystemDbContext()
@@ -18,6 +14,65 @@ namespace Warehouse.Data
         public static WarehouseSystemDbContext Create()
         {
             return new WarehouseSystemDbContext();
+        }
+
+        public IDbSet<Category> Categories { get; set; }
+
+        public IDbSet<Division> Divisions { get; set; }
+
+        public IDbSet<Image> Images { get; set; }
+
+        public IDbSet<Message> Messages { get; set; }
+
+        public IDbSet<OrderQuantity> OrderQuantities { get; set; }
+
+        public IDbSet<Organization> Organizations { get; set; }
+
+        public IDbSet<Product> Products { get; set; }
+
+        public IDbSet<PurchaseOrder> PurchaseOrders { get; set; }
+
+        public IDbSet<ScheduleOrder> ScheduleOrders { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<User>()
+                .HasRequired(p => p.Organization)
+                .WithMany(x => x.Employees)
+                .WillCascadeOnDelete(true);
+
+            modelBuilder
+                .Entity<PurchaseOrder>()
+                .HasRequired(p => p.Creator)
+                .WithMany(x => x.Orders)
+                .WillCascadeOnDelete(true);
+
+            modelBuilder
+                .Entity<Product>()
+                .HasRequired(p => p.Category)
+                .WithMany(x => x.Products)
+                .WillCascadeOnDelete(true);
+
+            modelBuilder
+                .Entity<Category>()
+                .HasRequired(p => p.Division)
+                .WithMany(x => x.Categories)
+                .WillCascadeOnDelete(true);
+
+            modelBuilder
+                .Entity<OrderQuantity>()
+                .HasRequired(p => p.Product)
+                .WithMany(x => x.OrderQuantities)
+                .WillCascadeOnDelete(true);
+
+            modelBuilder
+                .Entity<OrderQuantity>()
+                .HasRequired(p => p.PurchaseOrder)
+                .WithMany(x => x.OrderQuantities)
+                .WillCascadeOnDelete(true);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
