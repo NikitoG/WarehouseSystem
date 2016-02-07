cac0818c1af6dd5539cdd440ae38043a8b1e2f75
NikitoG <niki_spike@yahoo.com>
2016-02-07 11:20:46 +0200

    Update all NuGet Packages

--- Source/Web/WarehouseSystem.Web/Models/IdentityModels.cs
+++ Source/Web/WarehouseSystem.Web/Models/IdentityModels.cs
@@ -7,9 +7,9 @@ using Microsoft.AspNet.Identity.EntityFramework;
 namespace WarehouseSystem.Web.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
+    public class User : IdentityUser
     {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
@@ -18,16 +18,16 @@ namespace WarehouseSystem.Web.Models
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    public class WarehouseSystemDbContext : IdentityDbContext<User>
     {
-        public ApplicationDbContext()
+        public WarehouseSystemDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
 
-        public static ApplicationDbContext Create()
+        public static WarehouseSystemDbContext Create()
         {
-            return new ApplicationDbContext();
+            return new WarehouseSystemDbContext();
         }
     }
 }
